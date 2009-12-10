1c64a10046a8d90ead56ecaf2ddfa5e867c8b709
Flurin Egger <flurin@digitpaint.nl>
2009-12-10 16:25:50 +0100

    Add support for buildnumbers (the date). They are automatically extracted from git, it takes the date from the HEAD commit. If git is not found or git finds that this commit is actually a tagged with a version, it just displays the version defined in BUILD.

--- lib/skyline.rb
+++ lib/skyline.rb
@@ -1,3 +1,6 @@
+require 'pathname'
+require File.dirname(__FILE__) + "/skyline/version"
+
 # The Skyline contains all Skyline related (core) code and
 # defines some methods like the root (path) and version
 # of the Skyline core.
@@ -13,10 +16,10 @@ module Skyline
   
   # Shortcut for the current version
   #
-  # @return [String] The current version in the format x.x.x
+  # @return [String] The current version in the format x.x.x.x (the BUILD version is optional)
   def version
     Skyline::VERSION::STRING
   end
   module_function :version  
+  
 end
-
--- lib/skyline/version.rb
+++ lib/skyline/version.rb
@@ -1,13 +1,36 @@
 module Skyline
   # The defines the current Skyline version. 
-  # Edge will always be the next TINY number.
+  # 
   #
   # @private
   module VERSION
     MAJOR = 3
     MINOR = 0
     TINY = 7
+    BUILD = 0
+
+    # Some hackery to determine if we're on a development branch or not
+    # this sets BUILD to a timestamp 
+    begin
+      git_dir = File.dirname(__FILE__) + "/../../.git"
+      if File.exist?(git_dir)
+        if `git --git-dir=#{git_dir} describe --tags HEAD` =~ /^v\d+\.\d+\.\d+(\.\d+)?$/
+          build = nil
+        else
+          build = `git --git-dir=#{git_dir} show HEAD --format=format:"%ct" --quiet 2>&1`
+          if build =~ /\d+/
+            build = Time.at(build.to_i).strftime("%Y%m%d") 
+          else
+            build = nil
+          end
+        end
+      end
+    rescue RuntimeError => e
+    end
+    
+    # If no BUILD can be determined or we're on a version, it takes the last number
+    build = build || BUILD
  
-    STRING = [MAJOR, MINOR, TINY].join('.')
+    STRING = [MAJOR, MINOR, TINY, build == 0 ? nil : build].compact.join('.')
   end
 end
