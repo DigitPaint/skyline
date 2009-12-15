d2488e5af38cee6a5c346beec515d3317b215682
Flurin Egger <flurin@digitpaint.nl>
2009-12-15 15:13:56 +0100

    Make sure the FormbuilderWithErrors gets set before every request, otherwise it will fail when it gets reloaded.

--- config/initializers/rails_defaults.rb
+++ config/initializers/rails_defaults.rb
@@ -1,2 +1,6 @@
 ActionView::Base.field_error_proc = Proc.new {|html_tag, instance|  %(#{html_tag})}
-ActionView::Base.default_form_builder = Skyline::FormBuilderWithErrors
+
+# This has to happen on every request as the Constant may not be the same anymore (because of reloading)
+ActionController::Dispatcher.to_prepare(:form_builder) do
+  ActionView::Base.default_form_builder = Skyline::FormBuilderWithErrors
+end
