145f6c73793eacb8350a39dcd22cc9e29efce315
Flurin Egger <flurin@digitpaint.nl>
2009-12-17 16:23:55 +0100

    Make migrations work with SQLite3

--- db/migrate/20090610142139_add_skyline_tags_taggable_type.rb
+++ db/migrate/20090610142139_add_skyline_tags_taggable_type.rb
@@ -1,6 +1,6 @@
 class AddSkylineTagsTaggableType < ActiveRecord::Migration
   def self.up
-    add_column :skyline_tags, :taggable_type, :string, :null => false
+    add_column :skyline_tags, :taggable_type, :string, :null => false, :default => ""
     add_index :skyline_tags, [:taggable_type, :tag]
     execute("UPDATE skyline_tags SET taggable_type='Skyline::MediaFile'")
   end
