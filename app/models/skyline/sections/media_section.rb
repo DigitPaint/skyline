0131743238d04b2449ca65cc6e89e70dfc0990c4
Flurin Egger <flurin@digitpaint.nl>
2009-12-21 16:30:36 +0100

    Add (optional) link to Skyline::Sections::MediaSection.

--- app/models/skyline/sections/media_section.rb
+++ app/models/skyline/sections/media_section.rb
@@ -5,9 +5,13 @@ class Skyline::Sections::MediaSection < ActiveRecord::Base
   
   ALIGNMENT = %w{left right block_left block_right block_center}
   
-  referable_content :linked
-
+  # The media linked to this section
+  referable_content :media
+  
+  # An optional link
+  referable_content :link
+  
   validates_numericality_of :width, :height, :allow_nil => true
   
-  delegate :url, :external?, :to => :linked
+  delegate :url, :external?, :to => :media
 end
