449ef123c5ffeec7812c477ed24cfa55e0cdeed1
Flurin Egger <flurin@digitpaint.nl>
2009-12-24 13:53:38 +0100

    Update docs for yardoc.

--- lib/skyline/settings.rb
+++ lib/skyline/settings.rb
@@ -62,15 +62,13 @@ module Skyline::Settings
       f
     end
     
-    # a safe way to get a value of a setting and report a warning if it can't be found     #
-    #  instead of calling Setting[:setting_identifier].field directly use setting(:setting_identifier, :field)
-    # ==== Parameters
-    # setting_identifier<Symbol>:: the symbol of the settings page
-    # field<Symbol>:: the name of the setting
-    #
-    # ==== Returns
-    # Object:: the value of the setting or nil if not found
-    # --
+    # A safe way to get a value of a setting and report a warning if it can't be found  
+    # instead of calling Setting[:setting_identifier].field directly use setting(:setting_identifier, :field)
+    # 
+    # @param setting_identifier [Symbol] the symbol of the settings page
+    # @param field [Symbol] the name of the setting
+    # 
+    # @return [Object] the value of the setting or nil if not found
     def get(setting_identifier, field)
       if s = self[setting_identifier]
         if s.respond_to?(field)
@@ -81,14 +79,12 @@ module Skyline::Settings
       nil
     end
     
-    # a safe way to get a page from the settings     
-    # ==== Parameters
-    # setting_identifier<Symbol>:: the symbol of the settings page
-    # field<Symbol>:: the name of the setting that references a page_id
-    #
-    # ==== Returns
-    # Page:: the page if found or nil otherwise
-    # --
+    # a safe way to get a page from the settings  
+    # 
+    # @param setting_identifier [Symbol] the symbol of the settings page
+    # @param field [Symbol] the name of the setting that references a page_id
+    # 
+    # @return [Page, NilClass] The page if found, nil otherwise
     def get_page(setting_identifier, field)
       if page_id = self.get(setting_identifier, field)
         return Skyline::Page.find_by_id(page_id) if page_id.present?
