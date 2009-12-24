944cbcfbad922adf7d0eef6b9d8e993bd3b6b666
Flurin Egger <flurin@digitpaint.nl>
2009-12-24 12:50:01 +0100

    Added deprecation for Skyline::Renderer.

--- lib/skyline/deprecated.rb
+++ lib/skyline/deprecated.rb
@@ -1,10 +1,10 @@
 module Skyline
 
-  # @deprecated  
+  # @deprecated  Don't use Skyline::ContentItem anymore, use Skyline::BelongsToReferable (will be removed in 3.1.0)
   module ContentItem
     
     def self.included(base)
-      warn "[DEPRECATION] Don't use Skyline::ContentItem anymore, use Skyline::BelongsToReferable"
+      warn "[DEPRECATION] Don't use Skyline::ContentItem anymore, use Skyline::BelongsToReferable (will be removed in 3.1.0)"
       
       base.send(:include, Skyline::BelongsToReferable)
       
@@ -21,10 +21,10 @@ module Skyline
     end    
   end
   
-  # @deprecated
+  # @deprecated Don't use Skyline::Referable anymore, use Skyline::HasManyReferablesIn (will be removed in 3.1.0)
   module Referable
     def self.included(base)
-      warn "[DEPRECATION] Don't use Skyline::Referable anymore, use Skyline::HasManyReferablesIn"
+      warn "[DEPRECATION] Don't use Skyline::Referable anymore, use Skyline::HasManyReferablesIn (will be removed in 3.1.0)"
             
       base.send(:include, Skyline::HasManyReferablesIn)
       class << base 
@@ -33,22 +33,44 @@ module Skyline
     end
   end
   
-  # @deprecated  
+  # @deprecated Don't use Skyline::SectionItem anymore, use Skyline::Sections::Interface (will be removed in 3.1.0)
   module SectionItem
     def self.included(base)
-      warn "[DEPRECATION] Don't use Skyline::SectionItem anymore, use Skyline::Sections::Interface"
+      warn "[DEPRECATION] Don't use Skyline::SectionItem anymore, use Skyline::Sections::Interface (will be removed in 3.1.0)"
       
       base.send(:include, Skyline::Sections::Interface)
     end
   end
   
-  # @deprecated
+  # @deprecated Don't use Skyline::FormBuilderWithErrors anymore, use Skyline::FormBuilder (will be removed in 3.1.0)
   module FormBuilderWithErrors
     def self.included(base)
-      warn "[DEPRECATION] Don't use Skyline::FormBuilderWithErrors anymore, use Skyline::FormBuilder"
+      warn "[DEPRECATION] Don't use Skyline::FormBuilderWithErrors anymore, use Skyline::FormBuilder (will be removed in 3.1.0)"
       
       base.send(:include, Skyline::FormBuilder)
     end
+  end
+  
+  # @deprecated "[DEPRECATION] Don't use Skyline::Renderer anymore, use Skyline::Rendering::Renderer (will be removed in 3.1.0)"
+  class Skyline::Renderer < Skyline::Rendering::Renderer
+    class << self
+      %w{renderables register_renderables renderable_types helper}.each do |m|
+        define_method(m) do |*args|
+          deprecate!
+          super
+        end
+      end
+      
+      def deprecate!
+        warn "[DEPRECATION] Don't use Skyline::Renderer anymore, use Skyline::Rendering::Renderer (will be removed in 3.1.0)"
+      end
+    end
+    
+    def initialize(*args)
+      self.class.deprecate!
+      super
+    end
+    
   end
     
 end
