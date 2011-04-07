7d867dd16ee620f7673c48eca7d0a2bf6f92bdae
Luis Doubrava <luis@digitpaint.nl>
2011-04-07 15:39:19 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,40 @@
 ActionController::Routing::Routes.draw do |map|
-
+ 
   # =================
   # = Skyline url's =
   # =================
-  map.namespace :skyline, :path_prefix => "/skyline" do |skyline|
-    
-#    skyline.root :controller => "articles", :action => "index", :type => "skyline/page"
-    
+  map.namespace :skyline do |skyline|
+     
+    skyline.root :controller => "articles", :action => "index", :type => "skyline/page"
+     
     skyline.resources :articles, :collection => {:reorder => :put} do |page|
       page.resources :article_versions
       page.resources :variants, :member => {:force_edit => :put}
       page.resources :publications, :member => {:rollback => :post}
-#      page.resource :published_publication
+      page.resource :published_publication
     end
-    
+     
     # These are currently just used for polling
     skyline.resources :variants do |variant|
       variant.map "current_editor", :controller => "variant_current_editor", :action => "poll"
     end
-
+ 
     skyline.resource :authentication
     skyline.resources :users
-    
+     
     skyline.resources :sections
     skyline.resources :content_sections
     skyline.resources :content_items
     skyline.resources :link_section_links
     skyline.resources :redirects, :only => [:new]
-    
+     
     skyline.resources :locales, :only => [:show]
-
+ 
     skyline.namespace :media do |media|
       media.resources :dirs do |dirs|
-      	# Alert! Routes hard coded in app/views/skyline/media_files/_index.html.erb        
+        # Alert! Routes hard coded in app/views/skyline/media_files/_index.html.erb        
         dirs.resources :files
-        
+         
         dirs.connect 'data/:size/:name.:format',
             :controller => 'data',
             :action => 'show',
@@ -44,8 +44,8 @@ ActionController::Routing::Routes.draw do |map|
             :action => 'show',
             :conditions => { :method => :get }        
       end
-    end    
-    
+    end   
+     
     skyline.namespace :browser do |browser|
       browser.resources :images
       browser.resources :links
@@ -57,9 +57,9 @@ ActionController::Routing::Routes.draw do |map|
             media_dir.resources :media_files
           end
         end
-      end          
+      end         
     end
-    
+     
     skyline.namespace :content do |content|
       content.namespace :editors do |editors|
         editors.connect 'editable_list/:action/:id', :controller => "editable_list"
@@ -67,32 +67,16 @@ ActionController::Routing::Routes.draw do |map|
       end
     end
     skyline.connect 'content/:action/*types', :controller => "content"
-    
+     
     skyline.resources :settings, :except => [:create, :destroy]
-  
-    skyline.namespace :media do |media|
-      media.resources :dirs do |dirs|
-        dirs.resources :files
-
-        dirs.connect 'data/:size/:name.:format',
-            :controller => 'data',
-            :action => 'show',
-            :conditions => { :method => :get }
-        dirs.connect 'data/:name.:format',
-            :controller => 'data',
-            :action => 'show',
-            :conditions => { :method => :get }        
-      end
-    end  
-    
+     
   end
-  
-
+ 
   # ========================
   # = Implementation url's =
   # ========================
   #Media files data route
-
+   
   map.connect 'media/dirs/:media_dir_id/data/:size/:name.:format',
       :controller => 'skyline/site/media_files_data',
       :action => 'show',
@@ -101,7 +85,7 @@ ActionController::Routing::Routes.draw do |map|
       :controller => 'skyline/site/media_files_data',
       :action => 'show',
       :conditions => { :method => :get }
-            
+             
   # Default pages renderer.
   # map.connect '*url', :controller => "skyline/site/pages", :action => "show"
 end
