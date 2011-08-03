22b5b24ff05a8a83c4b1117b14ada290a0b66540
Flurin Egger <flurin@digitpaint.nl>
2011-08-03 15:54:26 +0200

    Remove debugging code

--- config/initializers/rails_defaults.rb
+++ config/initializers/rails_defaults.rb
@@ -2,9 +2,6 @@ Rails.application.config.action_view.field_error_proc = Proc.new {|html_tag, ins
 
 # This has to happen on every request as the Constant may not be the same anymore (because of reloading)
 ActionDispatch::Callbacks.to_prepare(:form_builder) do
-  Rails.logger.warn "---> Call me!"
   Rails.application.config.action_view.default_form_builder = Skyline::FormBuilder
   ActionView::Base.default_form_builder = Skyline::FormBuilder
-  
-
 end
