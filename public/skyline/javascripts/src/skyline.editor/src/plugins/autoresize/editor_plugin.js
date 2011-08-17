3c4bc9d37e08088c26aa748284f2bd0064fbe2b5
Flurin Egger <flurin@digitpaint.nl>
2011-08-17 10:43:59 +0200

    Update TinyMCE autoresize plugin

--- public/skyline/javascripts/src/skyline.editor/src/plugins/autoresize/editor_plugin.js
+++ public/skyline/javascripts/src/skyline.editor/src/plugins/autoresize/editor_plugin.js
@@ -23,46 +23,86 @@
 		* @param {string} url Absolute URL to where the plugin is located.
 		*/
 		init : function(ed, url) {
-			var t = this;
+			var t = this, oldSize = 0;
 
 			/**
 			 * This method gets executed each time the editor needs to resize.
 			 */
 			function resize() {
-				var d = ed.getDoc(), b = d.body, de = d.documentElement, DOM = tinymce.DOM, resizeHeight = t.autoresize_min_height, myHeight;
+        var d = ed.getDoc(), b = d.body, de = d.documentElement, DOM = tinymce.DOM, resizeHeight = t.autoresize_min_height, myHeight, ifr;
 
-				// Get height differently depending on the browser used
-				myHeight = tinymce.isIE ? b.scrollHeight : de.offsetHeight;
-
-				// Don't make it smaller than the minimum height
-				if (myHeight > t.autoresize_min_height)
-					resizeHeight = myHeight;
-
-        var ifr = DOM.get(ed.id + '_ifr');
-        var tbl = DOM.get(ed.id + '_tbl');
+        ifr = DOM.get(ed.id + '_ifr');
         
-  			// Calc difference between iframe and container
-        var dh = tbl.clientHeight - ifr.clientHeight;
+        if(tinymce.isWebKit){
+          DOM.setStyle(ifr, 'height', "0px");
+        }
+        
+        var parseMarginValue = function(v){
+          if(tinymce.is(v, 'string')){
+            if (v.indexOf('px') > -1){
+              v =  parseInt(v)
+            } else{
+              v = 0
+            }
+          } 
+          return v;
+        }
+        
+        //  We have to incorporate the margin too.
+        var vMargin = parseMarginValue(DOM.getStyle(d.body, "marginTop", true)) + parseMarginValue(DOM.getStyle(d.body, "marginBottom", true))
+        
+        // Get height differently depending on the browser used
+        myHeight = tinymce.isIE ? b.scrollHeight : d.body.offsetHeight;
 
-				// Resize content element
-				ed.getWin().scrollTo(0,0);				
-				DOM.setStyle(ifr, 'height', resizeHeight + 'px');    
+        // Don't make it smaller than the minimum height
+        if (myHeight > t.autoresize_min_height)
+        	resizeHeight = myHeight;
 
-			  // Resize the table too
-  			DOM.setStyle(tbl, 'height', resizeHeight + dh);
-  			
-  			ed.skyline_editor_height = resizeHeight;
+        // Resize content element
+        if (resizeHeight !== oldSize) {
+          ed.getWin().scrollTo(0,0); 
+        	DOM.setStyle(ifr, 'height', (resizeHeight + vMargin) + 'px');
+        	oldSize = resizeHeight;
+        } else {
+          if(tinymce.isWebKit){
+            DOM.setStyle(ifr, 'height', (oldSize + vMargin) + "px");
+          }          
+        }
+        
+        // if we're throbbing, we'll re-throb to match the new size
+        if (t.throbbing) {
+        	ed.setProgressState(false);
+        	ed.setProgressState(true);
+        }
+        
+        ed.skyline_editor_height = resizeHeight;
 			};
 
 			t.editor = ed;
 
 			// Define minimum height
       t.autoresize_min_height = 10;
-
+      
 			// Things to do when the editor is ready
 			ed.onInit.add(function(ed, l) {
 				// Hide scrollbars
-				ed.getBody().style.overflowY = "hidden";
+				var b = ed.getBody()
+        var tbl = tinymce.DOM.get(ed.id + '_tbl');
+				
+				b.style.overflowY = "hidden";
+        if(tinymce.isIE){
+          b.scroll = "no"  
+          tinymce.DOM.setStyle(tbl, "zoom", "1");          
+        }
+        
+        tinymce.DOM.setStyle(tbl, "table-layout", "fixed");
+        tinymce.DOM.setStyle(tbl, "height", "auto");       
+        
+        // Resize the window onload
+        var w = ed.getWin();
+        if(w.addEventListener){
+          w.addEventListener("load", resize, false)         
+        }
 			});
 			
 			var delayedResize = function(){
@@ -80,13 +120,18 @@
 			ed.onKeyUp.add(resize);
 			ed.onPostRender.add(resize);
 
+
+
 			ed.onLoadContent.add(function(ed, l) {
         // resize();
 
 				// Because the content area resizes when its content CSS loads,
-				// and we can't easily add a listener to its onload event,
-				// we'll just trigger a resize after a short loading period
+				// and we listen to a change in background color.
 				delayedResize();
+				if(tinymce.isIE){
+          // IE Caches too much.
+				  setTimeout(delayedResize, 150);
+				}
 			});
 
 			// Register the command so that it can be invoked by using tinyMCE.activeEditor.execCommand('mceExample');
