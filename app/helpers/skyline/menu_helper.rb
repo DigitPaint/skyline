c12733d0ea38e176e210376e1b1f09730bf3b205
Joran Kapteijns <joran@digitpaint.nl>
2012-02-22 17:02:57 +0100

    #140 - remove unused menu_for helper

--- app/helpers/skyline/menu_helper.rb
+++ app/helpers/skyline/menu_helper.rb
@@ -18,14 +18,6 @@ module Skyline::MenuHelper
     content_tag("li", link_to(t(scope.last, :scope => [:navigation, scope[-2]]), url, url_options), options)
   end
 
-  def menu_for(*scope, &block)
-    return unless controller.current_menu[0, scope.size] == scope
-    @_menu_scope = scope
-    menu = capture(&block)
-    @_menu_scope = []
-    menu
-  end
-  
   def current_menu_scope(scope)
     (@_menu_scope || []) + scope
   end
