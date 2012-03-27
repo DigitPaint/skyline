8b2e5445d244281f488ec0ef4197fa387119d1ed
Luis Doubrava <luis@digitpaint.nl>
2012-03-27 17:31:28 +0200

    fix migration issue for old skyline versions

--- db/migrate/20120327152625_remove_unused_articles_index.rb
+++ db/migrate/20120327152625_remove_unused_articles_index.rb
@@ -1,9 +1,9 @@
 class RemoveUnusedArticlesIndex < ActiveRecord::Migration
-  def self.up
+  def up
     remove_index :skyline_articles, :name => "sp_page_id_in_navigation"
   end
 
-  def self.down
-    # No backward, because this index is useless.
+  def down
+    add_index :skyline_pages, [:page_id, :in_navigation], :name => "sp_page_id_in_navigation"
   end
 end
