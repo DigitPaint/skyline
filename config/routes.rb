1d66a64dafc5d1318649a905b4a63f0b2439a09b
Joran Kapteijns <joran@digitpaint.nl>
2012-04-17 17:33:08 +0200

    Added configurable content_browser to allow selection of content as link

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Rails.application.routes.draw do
     namespace :browser do
       resources :images
       resources :links
+      resources :content
       resources :pages
       resources :files
       namespace :tabs do
@@ -57,7 +58,8 @@ Rails.application.routes.draw do
             resources :media_files
           end
         end
-        resources :linkables        
+        resources :linkables
+        resources :content_items
       end         
     end
      
