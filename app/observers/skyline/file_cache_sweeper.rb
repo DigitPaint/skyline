ac5a82605e3a7c9ddace4abe15f13f33d19cbf7d
Flurin Egger <flurin@digitpaint.nl>
2012-05-04 17:48:23 +0200

    Cache sweeper now uses full class names (needs migration)

--- app/observers/skyline/file_cache_sweeper.rb
+++ app/observers/skyline/file_cache_sweeper.rb
@@ -1,9 +1,9 @@
 class Skyline::FileCacheSweeper < ActiveRecord::Observer
-  observe Skyline::MediaFile, Skyline::MediaDir, Skyline::MediaNode
+  observe Skyline::MediaFile, Skyline::MediaDir
   
   def after_save(record)
     if record.renamed?
-    expire_for(record)
+      expire_for(record)
     end 
   end
   
@@ -14,10 +14,10 @@ class Skyline::FileCacheSweeper < ActiveRecord::Observer
   def expire_for(record)    
     case record
       when Skyline::MediaFile
-        Skyline::MediaCache.destroy_all("object_id = #{record.id} AND object_type = 'MediaFile'")
+        Skyline::MediaCache.destroy_all("object_id = #{record.id} AND object_type = 'Skyline::MediaFile'")
       when Skyline::MediaDir
         ids = record.files.collect{|f| f.id}.join(",")
-        Skyline::MediaCache.destroy_all("object_id IN (#{ids}) AND object_type = 'MediaFile'") unless ids.blank?
+        Skyline::MediaCache.destroy_all("object_id IN (#{ids}) AND object_type = 'Skyline::MediaFile'") unless ids.blank?
     end
   end
 end
