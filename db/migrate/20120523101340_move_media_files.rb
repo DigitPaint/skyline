54b28f041d4791cdeeb4fa9ef70e873674402cfa
Luis Doubrava <luis@digitpaint.nl>
2012-05-24 10:46:22 +0200

    fix migration so files are moved to a temporary folder first before being moved to their new location

--- db/migrate/20120523101340_move_media_files.rb
+++ db/migrate/20120523101340_move_media_files.rb
@@ -1,15 +1,26 @@
 class MoveMediaFiles < ActiveRecord::Migration
   
   def up
+    assets_path = Skyline::MediaNode.assets_path
+    Skyline::MediaFile.all.each do |file|
+      old_file_path = File.join(assets_path, file.id.to_s)
+      
+      if File.exist?(old_file_path)
+        new_file_key = file.id.to_s.reverse.ljust(4, "0")
+        new_file_path = File.join(assets_path, "tmp", [new_file_key[0,2], new_file_key[2,2]].join('/'), file.id.to_s)
+        FileUtils.makedirs(File.dirname(new_file_path))
+        File.rename old_file_path, new_file_path
+      end
+    end
+    
     Skyline::MediaFile.all.each do |file|
       assets_path = Skyline::MediaNode.assets_path
-    
-      old_file_path = File.join(assets_path, file.id.to_s)
-    
+      old_file_key = file.id.to_s.reverse.ljust(4, "0")
+      old_file_path = File.join(assets_path, "tmp", [old_file_key[0,2], old_file_key[2,2]].join('/'), file.id.to_s)
+      
       if File.exist?(old_file_path)
         new_file_key = file.id.to_s.reverse.ljust(4, "0")
         new_file_path = File.join(assets_path, [new_file_key[0,2], new_file_key[2,2]].join('/'), file.id.to_s)
-        
         FileUtils.makedirs(File.dirname(new_file_path))
         File.rename old_file_path, new_file_path
       end
