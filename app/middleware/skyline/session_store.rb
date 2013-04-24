97939cabded1d59cf89a417e3c9af64ffbecf08f
Ronald Wind <ronald@digitpaint.nl>
2013-04-24 11:21:02 +0200

    Fix Skyline::SesssionStore STI bug when there is no sessions table

--- app/middleware/skyline/session_store.rb
+++ app/middleware/skyline/session_store.rb
@@ -9,7 +9,7 @@ class Skyline::SessionStore < ActiveRecord::SessionStore
   
   class Session < ActiveRecord::SessionStore::Session
     self.table_name = "skyline_sessions"
-    
+
     # This method needs to be implemented as the superclass calls remove_method on it
     def self.find_by_session_id(session_id)
       find :first, :conditions => {:session_id=>session_id}
@@ -18,9 +18,25 @@ class Skyline::SessionStore < ActiveRecord::SessionStore
   end
   
   def self.session_class
+    self.fix_sessions_table_bug
+
     Skyline::SessionStore::Session
   end
-  
+
+  #
+  # On the first use of our own SessionStore Rails will initialize all attribute accessor methods based on our columns.
+  # No problem, because it can read that from the DB and our table 'skyline_session'.
+  # However, rails will first try to initialize accessor methods for our superclass ActiveRecord::SessionStore::Session.
+  # That will fail if there is no 'sessions' table, because the default table_name of the Rails SessionStore is 'sessions'.
+  # So we set the table_name to our table in this situation.
+  # <RW> I'm a bit unsure on were to call this function from.
+  #
+  def self.fix_sessions_table_bug
+    if !ActiveRecord::SessionStore::Session.table_exists? && ActiveRecord::SessionStore::Session.table_name == "sessions"
+      ActiveRecord::SessionStore::Session.table_name = "skyline_sessions"
+    end
+  end
+
   # As these functions use @@session_store in the original, overwrite them to be sure 
   # that yes, we really want to use our session store for Skyline only.
   private
@@ -42,5 +58,4 @@ class Skyline::SessionStore < ActiveRecord::SessionStore
       self.class.session_class.new(:session_id => id, :data => {})
   
   end
-  
 end
